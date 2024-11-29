14422b993c7c26d58ffed33971e7dd6e014e8614
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-29 16:49:16 -0500

    add index hoots

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -22,5 +22,17 @@ try {
 }
 });
 
+router.get('/', async (req, res) => {
+    try {
+        const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+        res.status(200).json(hoots);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 
 module.exports = router;
